67147f8b43541ecce71d591e102dba3be2c04df9
Alexandru Boghean <alexandrubgn98@gmail.com>
2023-06-24 11:24:47 +0300

    Censor-Email-Adress

---
+++ CensorEmail/Program.cs
@@ -0,0 +1,92 @@
+﻿// Censor the local-part of an email.
+// This program is manual validating the email format.
+
+
+public class CensorEmail
+{
+    public static void Main()
+    {
+        Console.Write("Your email adress is: ");
+        string userEmail = Console.ReadLine();
+        bool approveEmail = ValidateEmail(userEmail);
+
+        if(approveEmail) 
+        {
+            var censoredAdress = Censorship(userEmail);
+            Console.WriteLine(censoredAdress);
+        }
+        
+    }
+
+    private static string Censorship(string userEmail)
+    {
+        int arondIndex = userEmail.IndexOf('@');
+
+        string localPart = userEmail.Substring(0, arondIndex);
+        string domain = userEmail.Substring(arondIndex);
+        string censoredAdress = new string('*', localPart.Length);
+
+        return censoredAdress + domain;
+    }
+
+    public static bool ValidateEmail(string adress)
+    {
+        if(adress != null && adress.Count(c => c == '@') == 1)
+        {
+
+            var localPart = ValidateLocalPart(adress);
+            var domaine = ValidateEmailDomaine(adress);
+
+            return localPart && domaine;
+        }
+
+        Console.WriteLine("You entered an invalid email, or the email is in wrong format!");
+        return false;
+    }
+
+    private static bool ValidateEmailDomaine(string adress)
+    {
+        var domain = adress.Substring(adress.IndexOf('@') + 1);
+        if(domain.Count(c => c == '.') == 1)
+        {
+            return PointPosition(domain, '.') && OnlyLettersDomain(domain);
+        }
+
+        return false;
+    }
+
+    private static bool ValidateLocalPart(string adress)
+    {
+        var localPart = adress.Substring(0, adress.IndexOf('@'));
+        char[] unallowed = { '\"', '(', ')', ',', ':', ';', '<', '>', '@', '[', '\\', ']' };
+
+        foreach (char c in localPart)
+        {
+            if (Array.IndexOf(unallowed, c) != -1)
+                return false;
+        }
+
+        return true;
+    }
+
+    private static bool PointPosition(string domain, char point)
+    {
+        return (domain.IndexOf(point) != 0 && domain.IndexOf(point) != domain.Length - 1);
+    }
+
+    private static bool OnlyLettersDomain(string domain)
+    {
+        foreach(char ch in domain)
+        {
+            if (!(char.IsLetterOrDigit(ch) || ch == '.'))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    
+        
+}
--- SumWithError/Program.cs
+++ SumWithError/Program.cs
@@ -2,14 +2,15 @@
 {
     public static void Main()
     {
-        int limit = 5;
-        int[] numbers = new int[limit];
+        int limit = 5;  // Setez limita
+        int[] numbers = new int[limit]; // Initiez un array in care urmeaza sa stochez fiecare numar in parte
 
-        do
+        // Umplu array-ul cu numere (de la coada la cap)
+        do  
         {
-            numbers[limit-1] = ValidateInput();
-            limit--;
-        }while (limit > 0);
+            numbers[limit-1] = ValidateInput(); // pornesc de la pozitia 4 (adica limita - 1) si merg pana la pozitia 0.
+            limit--;  // Scade si limita de la 5 la 4 apoi 3, 2, 1. Cand limita e 1, algoritmul mai ruleaza inca odata si completeaza si pozitia 0.  
+        }while (limit > 0);  // Repeta procesul pana cand limita = 0. Daca limita ajunge la 0, se opreste (Do - While) -ul 
 
         int sum = 0;
         foreach (int i in numbers)
@@ -20,19 +21,22 @@
         Console.WriteLine(sum);
     }
 
-    public static int ValidateInput()
+    public static int ValidateInput()  // Validez fiecare input in parte, pe masura ce utilizatorul il introduce
     {
 
-        try
+        try  // incerca sa parseze numarul intr-un int; In cazul in care nu reuseste, si int.Parse da o eroare, se executa codul din catch
         {
-            int number = int.Parse(Console.ReadLine());
+            int number = int.Parse(Console.ReadLine());  // daca inputul este bun, si se reuseste Parse-ul la int, returneaza numarul care apoi va si memorat pe pozitia aferenta in array-ul de mai devreme
             return number;
         }
-        catch
+        catch  // Daca Parse -ul de mai devreme da erare pentru ca input-ul nu e in format corespunzator, executa codul de mai jos
         {
-            Console.WriteLine("You entered an invalid number. Try again!");
-            int number = int.Parse(Console.ReadLine());
-            return number;
+            Console.WriteLine("You entered an invalid number. Try again!"); // Display a message to try again.
+            int number = int.Parse(Console.ReadLine()); // Ii cer din nou utilizatorului un nou input
+            return number; // returnez numarul.
+
+            // Liniile de cod din Catch se executa o singura data, de asta utilizatorul e rugat o singura data sa introduca o alta valoare.
+            // Daca introduce din nou o valoare gresita, atunci programul va da o eroare si va opri programul.
         }
     }
 }
